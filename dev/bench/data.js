0b0b2f238702f21a2cdbede0755819394918b28b
github-action-benchmark <github@users.noreply.github.com>
2026-08-16 14:52:32 +0000

    add pgbench (heap) (customBiggerIsBetter) benchmark result for d5b71641e7688eef94c15f7932a0566934df2e20

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786891950624,
+  "lastUpdate": 1786891952305,
   "repoUrl": "https://github.com/ovr/crabgresql",
   "entries": {
     "ClickBench (parquet)": [
@@ -39496,6 +39496,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "read-only",
             "value": 18855.79014,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "talk@dmtry.me",
+            "name": "Dmitry Patsura",
+            "username": "ovr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d5b71641e7688eef94c15f7932a0566934df2e20",
+          "message": "feat: Support pg_is_in_recovery and the transaction-id functions (#254)",
+          "timestamp": "2026-08-16T16:04:02+02:00",
+          "tree_id": "0361b397bb491760f6ad278218401431703f132f",
+          "url": "https://github.com/ovr/crabgresql/commit/d5b71641e7688eef94c15f7932a0566934df2e20"
+        },
+        "date": 1786891952194,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tpcb-like",
+            "value": 1201.208118,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          },
+          {
+            "name": "read-only",
+            "value": 38588.809243,
             "unit": "tps",
             "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
           }
