151d13a428e72016ed156ca5ca414ecd45fe30cd
github-action-benchmark <github@users.noreply.github.com>
2026-08-20 13:41:27 +0000

    add pgbench (heap) (customBiggerIsBetter) benchmark result for 75421d22fe4e1d54e9322cec02a7d02eed1a015b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787233285658,
+  "lastUpdate": 1787233287460,
   "repoUrl": "https://github.com/ovr/crabgresql",
   "entries": {
     "ClickBench (parquet)": [
@@ -45904,6 +45904,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "read-only",
             "value": 20181.779921,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "talk@dmtry.me",
+            "name": "Dmitry Patsura",
+            "username": "ovr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "75421d22fe4e1d54e9322cec02a7d02eed1a015b",
+          "message": "feat: Support pg_class.relfilenode (#263)",
+          "timestamp": "2026-08-20T14:51:28+02:00",
+          "tree_id": "77f4465d499c216cba7cdb3f89e73ba30315261f",
+          "url": "https://github.com/ovr/crabgresql/commit/75421d22fe4e1d54e9322cec02a7d02eed1a015b"
+        },
+        "date": 1787233287326,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tpcb-like",
+            "value": 1265.120764,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          },
+          {
+            "name": "read-only",
+            "value": 18448.81309,
             "unit": "tps",
             "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
           }
