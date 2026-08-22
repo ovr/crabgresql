48e3ae1cb36f07a0d89a4675d8f314682f778aee
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 14:52:10 +0000

    add pgbench (heap) (customBiggerIsBetter) benchmark result for 4135d446c98f59ddfebc7dc2bcdf42bbd50dce02

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787410328734,
+  "lastUpdate": 1787410330286,
   "repoUrl": "https://github.com/ovr/crabgresql",
   "entries": {
     "ClickBench (parquet)": [
@@ -50440,6 +50440,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "read-only",
             "value": 18247.346039,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "talk@dmtry.me",
+            "name": "Dmitry Patsura",
+            "username": "ovr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4135d446c98f59ddfebc7dc2bcdf42bbd50dce02",
+          "message": "feat: Support pg_get_viewdef(oid), pg_get_ruledef and pg_get_function_* (#272)",
+          "timestamp": "2026-08-22T16:01:11+02:00",
+          "tree_id": "44d11ed40973a1600bc0c130e93f90e12013f727",
+          "url": "https://github.com/ovr/crabgresql/commit/4135d446c98f59ddfebc7dc2bcdf42bbd50dce02"
+        },
+        "date": 1787410330170,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tpcb-like",
+            "value": 1141.851962,
+            "unit": "tps",
+            "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
+          },
+          {
+            "name": "read-only",
+            "value": 18615.751098,
             "unit": "tps",
             "extra": "scale 10, 4 clients, 60s, shared_buffers=2GB"
           }
